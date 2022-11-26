3a319bb89c498bc2a2f3509738f400a0745cf987
Clover <cloveryou02@163.com>
2022-11-26 18:33:20 +0800

    业务异常基类

---
+++ src/main/java/top/ctong/commerce/smartcommerce/exceptions/BaseException.java
@@ -0,0 +1,29 @@
+package top.ctong.commerce.smartcommerce.exceptions;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2022 Clover You.
+ * <p>
+ * 业务异常基类
+ * </p>
+ * @author Clover You
+ * @email cloveryou02@163.com
+ * @create 2022-11-26 16:44
+ */
+public abstract class BaseException extends RuntimeException{
+
+    private static final long serialVersionUID = 352653175534763518L;
+
+
+    public BaseException() {
+
+    }
+}
