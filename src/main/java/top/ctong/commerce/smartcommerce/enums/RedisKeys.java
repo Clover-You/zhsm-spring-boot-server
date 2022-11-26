dea647943abb1e229325493ea8a5f676bee37994
Clover <cloveryou02@163.com>
2022-11-26 13:36:54 +0800

    the cache key of the code when the user register with the mailbox

---
+++ src/main/java/top/ctong/commerce/smartcommerce/enums/RedisKeys.java
@@ -0,0 +1,33 @@
+package top.ctong.commerce.smartcommerce.enums;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2022 Clover You.
+ * <p>
+ * redis key
+ * </p>
+ * @author Clover You
+ * @email cloveryou02@163.com
+ * @create 2022-11-26 13:29
+ */
+public enum RedisKeys {
+
+    /**
+     * 用户注册
+     */
+    USER_REGISTER_EMAIL_CODE("user_register_email_code:");
+
+    private String key;
+
+    private RedisKeys(String key) {
+        this.key = key;
+    }
+}
