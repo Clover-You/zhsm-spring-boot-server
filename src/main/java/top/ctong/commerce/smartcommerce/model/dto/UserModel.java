db43b702d2a0d57b244cee1aaa1c49c490849953
Clover <cloveryou02@163.com>
2022-11-26 13:37:28 +0800

    user info model

--- src/main/java/top/ctong/commerce/smartcommerce/model/dto/UserModel.java
+++ src/main/java/top/ctong/commerce/smartcommerce/model/dto/UserModel.java
@@ -1,4 +1,14 @@
-package top.ctong.commerce.smartcommerce.model.dto;/**
+package top.ctong.commerce.smartcommerce.model.dto;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+/**
  * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
  * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
  * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
@@ -15,5 +25,52 @@ package top.ctong.commerce.smartcommerce.model.dto;/**
  * @author Clover You
  * @email 2621869236@qq.com
  * @create 2022-11-26 12:44
- */public class UserModel {
+ */
+@Slf4j
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("sys_user")
+public class UserModel {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户id
+     */
+    private String userId;
+
+    /**
+     * 昵称
+     */
+    private String nickName;
+
+    /**
+     * 密码
+     */
+    private String pass;
+
+    /**
+     * 密码随机盐值
+     */
+    private String salt;
+
+    /**
+     * 头像地址
+     */
+    private String avatar;
+
+    /**
+     * 是否被禁止登录
+     */
+    private Boolean isBanned;
+
+    /**
+     * 用户创建日期
+     */
+    private Date createAt;
+
+    /**
+     * 删除日期
+     */
+    private Date delAt;
 }
