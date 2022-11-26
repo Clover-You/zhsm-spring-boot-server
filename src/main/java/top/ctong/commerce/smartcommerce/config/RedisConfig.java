3b61e98fea1d3f5badeb827f0f6ac0d9b7e17d17
Clover <cloveryou02@163.com>
2022-11-26 18:34:17 +0800

    redis config file

--- src/main/java/top/ctong/commerce/smartcommerce/config/RedisConfig.java
+++ src/main/java/top/ctong/commerce/smartcommerce/config/RedisConfig.java
@@ -1,4 +1,15 @@
-package top.ctong.commerce.smartcommerce.config;/**
+package top.ctong.commerce.smartcommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
  * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
  * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
  * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
@@ -15,5 +26,17 @@ package top.ctong.commerce.smartcommerce.config;/**
  * @author Clover You
  * @email cloveryou02@163.com
  * @create 2022-11-26 16:22
- */public class RedisConfig {
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> rt = new RedisTemplate<>();
+        rt.setConnectionFactory(redisConnectionFactory);
+
+        rt.setKeySerializer(new StringRedisSerializer());
+        rt.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return rt;
+    }
 }
