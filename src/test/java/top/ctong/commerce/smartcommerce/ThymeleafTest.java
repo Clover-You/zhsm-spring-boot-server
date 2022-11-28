dfba2f2c65282ee2d9a94726e7362b1c10b6c8e4
Clover <cloveryou02@163.com>
2022-11-28 16:40:31 +0800

    add thymeleaf engine test

--- src/test/java/top/ctong/commerce/smartcommerce/ThymeleafTest.java
+++ src/test/java/top/ctong/commerce/smartcommerce/ThymeleafTest.java
@@ -1,5 +1,18 @@
 package top.ctong.commerce.smartcommerce;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.nio.charset.StandardCharsets;
+
 /**
  * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
  * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
@@ -18,5 +31,29 @@ package top.ctong.commerce.smartcommerce;
  * @email cloveryou02@163.com
  * @create 2022-11-28 15:04
  */
-public class ThymelafyTest {
+@Slf4j
+@SpringBootTest
+public class ThymeleafTest {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    @DisplayName("engine")
+    void text01() {
+//        TemplateEngine templateEngine = new SpringTemplateEngine();
+//        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+//        resolver.setPrefix("/templates/");
+//        resolver.setSuffix(".html");
+//        resolver.setTemplateMode(TemplateMode.HTML);
+//        resolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+//        templateEngine.setTemplateResolver(resolver);
+
+        Context context = new Context();
+        context.setVariable("code", 123);
+
+        String process = templateEngine.process("email", context);
+        log.info(process);
+    }
+
 }
