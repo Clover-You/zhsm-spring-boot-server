449ab3b9d1b6bf4ec142793bbd23b4305200a259
Clover <cloveryou02@163.com>
2022-11-28 16:38:04 +0800

    feat: add global exception handler

--- src/main/java/top/ctong/commerce/smartcommerce/controller/advice/DefaultAdvice.java
+++ src/main/java/top/ctong/commerce/smartcommerce/controller/advice/DefaultAdvice.java
@@ -1,8 +1,16 @@
 package top.ctong.commerce.smartcommerce.controller.advice;
 
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import top.ctong.commerce.smartcommerce.Util.R;
+import top.ctong.commerce.smartcommerce.utils.R;
+import top.ctong.commerce.smartcommerce.enums.RespStatus;
+import top.ctong.commerce.smartcommerce.exceptions.BaseException;
+
+import java.util.Locale;
 
 /**
  * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
@@ -22,16 +30,27 @@ import top.ctong.commerce.smartcommerce.Util.R;
  * @email cloveryou02@163.com
  * @create 2022-08-07 3:46 AM
  */
-//@ControllerAdvice
+@ControllerAdvice("top.ctong.commerce.smartcommerce.controller")
 public class DefaultAdvice {
-    
-    /** 
+
+    @Setter(onMethod = @__(@Autowired))
+    private MessageSource messageSource;
+    /**
      * 默认异常处理器
      * @author Clover
      * @date 2022/8/7 3:48 AM
      */
-//    @ExceptionHandler
-//    public R defaultHandler() {
-//        return R.fail();
-//    }
+    @ExceptionHandler({BaseException.class})
+    public R baseExceptionHandler(BaseException e) {
+        Locale locale = LocaleContextHolder.getLocale();
+        RespStatus hs = e.getHs();
+        String message = messageSource.getMessage(hs.getMsg(), e.getI18nArgs(), locale);
+
+        return R.fail();
+    }
+
+    @ExceptionHandler({Exception.class})
+    public R defaultExceptionHandler(Exception e) {
+        return R.fail();
+    }
 }
