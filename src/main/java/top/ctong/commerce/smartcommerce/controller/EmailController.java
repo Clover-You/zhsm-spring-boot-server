fbcc05b9141f66cdae514b2fd74da6cba1d5675d
Clover <cloveryou02@163.com>
2022-12-02 15:53:56 +0800

    add exception handler

--- src/main/java/top/ctong/commerce/smartcommerce/controller/EmailController.java
+++ src/main/java/top/ctong/commerce/smartcommerce/controller/EmailController.java
@@ -2,19 +2,14 @@ package top.ctong.commerce.smartcommerce.controller;
 
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import top.ctong.commerce.smartcommerce.enums.RespStatus;
+import top.ctong.commerce.smartcommerce.exceptions.FrequentOperationException;
 import top.ctong.commerce.smartcommerce.model.dto.UserRegisterCodeDto;
 import top.ctong.commerce.smartcommerce.service.EmailService;
 import top.ctong.commerce.smartcommerce.utils.R;
-
-import javax.mail.*;
-import java.util.Locale;
 
 /**
  * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
@@ -51,7 +46,7 @@ public class EmailController {
      * @date 2022/8/7 2:56 AM
      */
     @GetMapping("/user/register/send_register_code")
-    public R userRegisterCode(UserRegisterCodeDto dto) throws MessagingException {
+    public R userRegisterCode(UserRegisterCodeDto dto) throws FrequentOperationException {
         emailService.userRegisterCode(dto.getEmailNo());
         return R.ok();
     }
